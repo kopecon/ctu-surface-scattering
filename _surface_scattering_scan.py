f7b6452124da031cc8a47b4100b0a2bdbcac3a10
lain157 <lain157@seznam.cz>
2025-02-14 14:23:25 +0100

    Renamed variables.

--- _surface_scattering_scan.py
+++ _surface_scattering_scan.py
@@ -7,14 +7,17 @@ import numpy as np
 import time
 from datetime import datetime
 
+import warnings
+
+
 # NI-DAQmx 2025 Q1 has to be installed on the executing pc to run the scan.
 # NI-DAQmx 2025 Q1 download: https://www.ni.com/en/support/downloads/drivers/download.ni-daq-mx.html?srsltid=AfmBOooJ-Ko5HpJHEr4yPzQEZKufBWdBc1JjWhomtdJ27QKXivgjvTBr#559060
 # TODO: Specify exceptions
 
 
 def _find_range(start, stop, step):
-    dx = int((stop - start) / step)
-    return np.linspace(start, stop, endpoint=True, num=dx + 1)
+    dx = int(abs((stop - start)) / step + 1)
+    return np.linspace(start, stop, endpoint=True, num=dx)
 
 
 def _find_range_double(step):
@@ -47,10 +50,10 @@ def _days_hours_minutes_seconds(dt):
 
 
 def scan_1d(motors, input_data, on_progress, on_progress2):
-    _motor_1 = motors[1]
-    _motor_2 = motors[2]
-    _motor_3 = motors[3]
-    _progress_count = 0
+    motor_1 = motors[1]
+    motor_2 = motors[2]
+    motor_3 = motors[3]
+    progress_count = 0
     print("1D measurement in progress...")
     # _____________________________________Cycle___________________________________
     angles = [0, 0, 0]
@@ -58,44 +61,38 @@ def scan_1d(motors, input_data, on_progress, on_progress2):
     name = datetime.utcnow().strftime("%Y%m%d_%H%M%S") + "_" + ".csv"
     print("Writing", name)
 
-    f1 = input_data[0]
-    f1 = float(f1)
+    m1_from = float(input_data[0])
     s1 = 1
 
-    f2 = input_data[3]
-    f2 = float(f2)
+    m2_from = float(input_data[3])
     s2 = 1
 
-    s3 = input_data[8]
-    s3 = float(s3)
+    m3_step = float(input_data[8])
 
-    maximum = (len(_find_range(f1, f1, s1))
-               * len(_find_range(f2, f2, s2))
-               * len(_find_range_double(s3)))
+    maximum = (len(_find_range(m1_from, m1_from, s1))
+               * len(_find_range(m2_from, m2_from, s2))
+               * len(_find_range_double(m3_step)))
     print("Max = ", maximum)
 
-    for i in _find_range(f1, f1, s1):
-        _motor_1.move_to_position(i)
+    for i in _find_range(m1_from, m1_from, s1):
+        motor_1.move_to_position(i)
 
-        angles.pop(0)
-        angles.insert(0, i)
+        angles[0] = i
 
         time.sleep(2)
 
-        for j in _find_range(f2, f2, s2):
-            _motor_2.move_to_position(j)
+        for j in _find_range(m2_from, m2_from, s2):
+            motor_2.move_to_position(j)
 
-            angles.pop(1)
-            angles.insert(1, j)
+            angles[1] = j
 
             time.sleep(2)
 
-            for k in _find_range_double(s3):
+            for k in _find_range_double(m3_step):
                 c1 = time.time()
-                _motor_3.move_to_position(k)
+                motor_3.move_to_position(k)
 
-                angles.pop(2)
-                angles.insert(2, k)
+                angles[2] = k
 
                 m1 = angles[0]
                 m2 = angles[1]
@@ -103,26 +100,20 @@ def scan_1d(motors, input_data, on_progress, on_progress2):
                 print("set angles")
 
                 with nidaqmx.Task() as task:
-                    task.ai_channels.add_ai_voltage_chan(
-                        "myDAQ1/ai0:1"
-                    )
+                    task.ai_channels.add_ai_voltage_chan("myDAQ1/ai0:1")
                     task.timing.cfg_samp_clk_timing(
-                        100000,
-                        source="",
-                        active_edge=Edge.RISING,
-                        sample_mode=AcquisitionType.FINITE,
-                        samps_per_chan=10,
-                    )
+                        100000, source="", active_edge=Edge.RISING, sample_mode=AcquisitionType.FINITE,
+                        samps_per_chan=10)
 
                     n = 0
-                    num = input_data[9]
-                    print("num = ", int(num))
-                    print(type(num))
+                    measurement_points = input_data[9]
+                    print("num = ", int(measurement_points))
+                    print(type(measurement_points))
 
                     column_names = ["a", "b", "c", "d", "e"]
                     df = pd.DataFrame(columns=column_names)
 
-                    while n < int(num):
+                    while n < int(measurement_points):
                         aaa = task.read()
                         data = {
                             "a": [m1],
@@ -147,18 +138,19 @@ def scan_1d(motors, input_data, on_progress, on_progress2):
                 print("Pomer je:", pomer)
 
                 with open(name, "a") as f:
-                    line = "{};{};{};{};{};{}".format(df2.iloc[0], df2.iloc[1], df2.iloc[2], df2.iloc[3], df2.iloc[4], pomer)
+                    line = "{};{};{};{};{};{}".format(df2.iloc[0], df2.iloc[1], df2.iloc[2], df2.iloc[3], df2.iloc[4],
+                                                      pomer)
                     print(line, file=f)
 
-                _update_progress(_progress_count, 1)
-                print("Progres count: ", _progress_count)
-                actual_progress = (100 / maximum) * _progress_count
+                _update_progress(progress_count, 1)
+                print("Progres count: ", progress_count)
+                actual_progress = (100 / maximum) * progress_count
                 print("Progress: ", actual_progress, " %")
                 on_progress.emit(actual_progress)  # vyslani signalu
 
                 c2 = time.time()
                 dt = c2 - c1
-                dn = maximum - _progress_count
+                dn = maximum - progress_count
                 dm = dt * dn
                 dm = (dm / 20) + dm  # +20% na prejezdy M1 a M2
                 print("dn = ", dn)
@@ -185,10 +177,10 @@ def scan_1d(motors, input_data, on_progress, on_progress2):
 
 
 def scan_3d(motors, input_data, on_progress, on_progress2):
-    _motor_1 = motors[1]
-    _motor_2 = motors[2]
-    _motor_3 = motors[3]
-    _progress_count = 0
+    motor_1 = motors[1]
+    motor_2 = motors[2]
+    motor_3 = motors[3]
+    progress_count = 0
 
     print("3D measurement in progress...")
 
@@ -198,122 +190,104 @@ def scan_3d(motors, input_data, on_progress, on_progress2):
     name = datetime.utcnow().strftime("%Y%m%d_%H%M%S") + "_" + ".csv"
     print("Writing", name)
 
-    f1 = input_data[0]
-    f1 = float(f1)
-    t1 = input_data[1]
-    t1 = float(t1)
-    s1 = input_data[2]
-    s1 = float(s1)
+    motor_1_from = float(input_data[0])
+    motor_1_to = float(input_data[1])
+    motor_1_step = float(input_data[2])
+    motor_1_range = _find_range(motor_1_from, motor_1_to, motor_1_step)
 
-    f2 = input_data[3]
-    f2 = float(f2)
-    t2 = input_data[4]
-    t2 = float(t2)
-    s2 = input_data[5]
-    s2 = float(s2)
+    motor_2_from = float(input_data[3])
+    motor_2_to = float(input_data[4])
+    motor_2_step = float(input_data[5])
+    motor_2_range = _find_range(motor_2_from, motor_2_to, motor_2_step)
 
-    f3 = input_data[6]
-    f3 = float(f3)
-    t3 = input_data[7]
-    t3 = float(t3)
-    s3 = input_data[8]
-    s3 = float(s3)
+    motor_3_from = float(input_data[6])
+    motor_3_to = float(input_data[7])
+    motor_3_step = float(input_data[8])
+    motor_3_range = _find_range(motor_3_from, motor_3_to, motor_3_step)
+    print("Motor 3 range:", motor_3_range)
 
-    maximum = (len(_find_range(f1, t1, s1))
-               * len(_find_range(f2, t2, s2))
-               * len(_find_range(f3, t3, s3)))
-    print("Max = ", maximum)
+    full_range = (len(motor_1_range) * len(motor_2_range) * len(motor_3_range))
 
-    for i in _find_range(f1, t1, s1):
-        print(_motor_1)
-        _motor_1.move_to_position(i)
+    number_of_measurement_points = input_data[9]
 
-        angles.pop(0)
-        angles.insert(0, i)
+    for i in motor_1_range:
+        motor_1.move_to_position(i)
+
+        angles[0] = i
 
         time.sleep(2)
 
-        for j in _find_range(f2, t2, s2):
-            print("self.motor 2: ", f2, " ", t2, " ", s2)
-            _motor_2.move_to_position(j)
+        for j in motor_2_range:
+            motor_2.move_to_position(j)
 
-            angles.pop(1)
-            angles.insert(1, j)
+            angles[1] = j
 
             time.sleep(2)
 
-            for k in _find_range(f3, t3, s3):
+            for k in motor_3_range:
                 c1 = time.time()
-                _motor_3.move_to_position(k)
+                motor_3.move_to_position(k)
 
-                angles.pop(2)
-                angles.insert(2, k)
+                angles[2] = k
 
-                m1 = angles[0]
-                m2 = angles[1]
-                m3 = angles[2]
-                print("set angles")
+                motor_1_position = angles[0]
+                motor_2_position = angles[1]
+                motor_3_position = angles[2]
 
-                try:
-                    with nidaqmx.Task() as task:
-                        task.ai_channels.add_ai_voltage_chan(
-                            "myDAQ1/ai0:1"
-                        )
-                        task.timing.cfg_samp_clk_timing(
-                            100000,
-                            source="",
-                            active_edge=Edge.RISING,
-                            sample_mode=AcquisitionType.FINITE,
-                            samps_per_chan=10,
-                        )
+                with nidaqmx.Task() as task:
+                    task.ai_channels.add_ai_voltage_chan("myDAQ1/ai0:1")
+                    task.timing.cfg_samp_clk_timing(
+                        100000,
+                        source="",
+                        active_edge=Edge.RISING,
+                        sample_mode=AcquisitionType.FINITE,
+                        samps_per_chan=10)
 
-                        n = 0
-                        num = input_data[9]
+                n = 0
 
-                        column_names = ["a", "b", "c", "d", "e"]
-                        df = pd.DataFrame(columns=column_names)
+                column_names = ["a", "b", "c", "d", "e"]
+                measurement_data = pd.DataFrame(columns=column_names)
 
-                        while n < int(num):
-                            aaa = task.read()
-                            data = {
-                                "a": [m1],
-                                "b": [m2],
-                                "c": [m3],
-                                "d": [aaa[0]],
-                                "e": [aaa[1]],
-                            }
-                            dp = pd.DataFrame(data)
-                            df = pd.concat((df, dp), axis=0)
-                            n += 1
+                while n < int(number_of_measurement_points):
+                    sensor_data = [420, 69]  # task.read()
+                    data = {
+                        "a": [motor_1_position],
+                        "b": [motor_2_position],
+                        "c": [motor_3_position],
+                        "d": [sensor_data[0]],
+                        "e": [sensor_data[1]]}
+                    current_scan = pd.DataFrame(data)
+                    measurement_data = pd.concat((measurement_data, current_scan), axis=0)
 
-                except:  # TODO Specify exception
-                    pass
-                df2 = df.mean()
-                print("m1:", df2[0], " m2:", df2[1], " m3:", df2[2])
+                    # To prevent pandas FutureWarning spam:
+                    warnings.simplefilter(action='ignore', category=FutureWarning)
+
+                    n += 1
+
+                df2 = measurement_data.mean()
+                print("m1:", df2.iloc[0], " m2:", df2.iloc[1], " m3:", df2.iloc[2])
                 print(
                     "prumer Signal1:",
-                    df2[3],
+                    df2.iloc[3],
                     " a prumer Signal2:",
-                    df2[4],
+                    df2.iloc[4],
                 )
-                pomer = df2[3] / df2[4]
+                pomer = df2.iloc[3] / df2.iloc[4]
                 print("Pomer je:", pomer)
 
                 with open(name, "a") as f:
-                    line = "{};{};{};{};{};{}".format(df2[0], df2[1], df2[2], df2[3], df2[4], pomer)
+                    line = "{};{};{};{};{};{}".format(df2.iloc[0], df2.iloc[1], df2.iloc[2], df2.iloc[3], df2.iloc[4], pomer)
                     print(line, file=f)
 
-                _update_progress(_progress_count, 1)
-                print("Progres count: ", _progress_count)
-                actual_progress = (100 / maximum) * _progress_count
+                _update_progress(progress_count, 1)
+                print("Progres count: ", progress_count)
+                actual_progress = (100 / full_range) * progress_count
                 print("Progress: ", actual_progress, " %")
-                on_progress.emit(
-                    actual_progress
-                )  # vyslani signalu
+                on_progress.emit(actual_progress)  # vyslani signalu
 
                 c2 = time.time()
                 dt = c2 - c1
-                dn = maximum - _progress_count
+                dn = full_range - progress_count
                 dm = dt * dn
                 dm = (dm / 20) + dm  # +20% na prejezdy M1 a M2
                 print("dn = ", dn)
@@ -337,3 +311,10 @@ def scan_3d(motors, input_data, on_progress, on_progress2):
                     seconds,
                     "s",
                 )
+    print("Done")
+
+
+if __name__ == '__main__':
+    scan_3d([None, None, None, None],
+            ['0', '90', '30', '90', '180', '30', '0', '90', '30', '3', False],
+            1, 2)
