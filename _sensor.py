09f82fb2f78a751d96dc85722384553d8ccf235d
lain157 <105891295+Lain157@users.noreply.github.com>
2025-03-16 19:46:50 +0100

    Removed if controller condition and added Try block.

--- _sensor.py
+++ _sensor.py
@@ -10,8 +10,8 @@ from nidaqmx.constants import Edge, AcquisitionType
 # NI-DAQmx 2025 Q1 download: https://www.ni.com/en/support/downloads/drivers/download.ni-daq-mx.html?srsltid=AfmBOooJ-Ko5HpJHEr4yPzQEZKufBWdBc1JjWhomtdJ27QKXivgjvTBr#559060
 
 
-def measure_scattering(controller):
-    if controller is not None:
+def measure_scattering():
+    try:
         with nidaqmx.Task() as task:
             task.ai_channels.add_ai_voltage_chan(
                 "myDAQ1/ai0:1"
@@ -29,5 +29,6 @@ def measure_scattering(controller):
             sensor_data_2 = float(sensor_data[1])
 
             return sensor_data_1, sensor_data_2
-    else:
+    except nidaqmx.errors.DaqNotFoundError:
+        print("Controller not found. Returning random data.")
         return random.randint(0, 100), random.randint(101, 200)
