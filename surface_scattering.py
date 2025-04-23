634f21083cf1fbb990fbb3b00bcd6f619caf1668
lain157 <lain157@seznam.cz>
2025-04-23 16:37:28 +0200

    Added todo list. Rounding scan motor positions to 1 decimal. THIS IS WORKING VERSION. But needs to be tested more.

--- surface_scattering.py
+++ surface_scattering.py
@@ -5,6 +5,13 @@ from PySide6.QtWidgets import QApplication
 import _gui
 
 
+# FIXME: Scan type does not reset along with the GUI
+# TODO: motor 3 from 90 60 30 0 270 300 330 does not graph properly
+# FIXME: Motor 3 2 1 positions display in GUI is stuck
+# TODO: Solve periodical data measurement for real time graph
+# FIXME: Doesnt graph motor 1 at 60 degrees
+# FIXME: Home button clears graphs
+# TODO: Switch from rounding to is close
 if __name__ == '__main__':
     # Create the Qt Application
     app = QApplication([])
